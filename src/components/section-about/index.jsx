45c004fa1a0f38b4a47531c02caf4429b93e5791
Tom Pollak <tompollak1000@gmail.com>
2022-04-25 11:25:02 +0100

    added more padding below about

--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -8,7 +8,7 @@ const SectionAbout = ({ about }) => {
   const github = "https://github.com/tom-pollak"
   return (
     <Section>
-      <div className="mb-6">
+      <div className="mb-8">
         <p><span className='font-extrabold inline text-3xl pr-3 text-gray-800'>Hi!</span>
         {split[0]}
         <a className="text-blue-500 hover:text-pink-500" href={github}>
