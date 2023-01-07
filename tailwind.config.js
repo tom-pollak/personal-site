9dc6bba5a566bacfcb9fee7db34cdd896f103e9f
tom-pollak <tom-pollak@users.noreply.github.com>
2023-01-07 23:56:34 +0000

    deploy: 12df8fac41e6b35fef2fee4d686731c4dc80504e

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+    // './**/*.html',
+    // './_site/**/*.html'
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
